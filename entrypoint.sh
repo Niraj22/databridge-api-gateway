f89173ec043d0eb29bc819f372ddb1527c18ea65
Niraj Budhathoki <nirajbudhathoki3@gmail.com>
2025-04-13 19:03:47 +0545

    update gateway

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,15 @@
-#!/bin/bash
+#!/bin/sh
 set -e
-rm -f /app/tmp/pids/server.pid
+
+if [ -f tmp/pids/server.pid ]; then
+  rm tmp/pids/server.pid
+fi
+
+echo 'Installing deps' \
+  && bundle install
+
+# Make bin/dev executable
+chmod +x bin/dev
+
+# Execute the command passed to docker run
 exec "$@"
